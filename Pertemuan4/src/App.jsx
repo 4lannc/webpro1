c6d71a5ad35df57b17c51b6e78bef6af501d08dc
4lannc <152788249+4lannc@users.noreply.github.com>
2024-11-15 21:54:08 +0700

    Add files via upload

---
+++ Pertemuan4/src/App.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <div className="text-2xl flex justify-center bg-blue-400 min-h-screen items-center">
+      <div>
+        <button
+        className="h-10 px-6 font-semibold rounded-md bg-sky-950 text-white"
+        type="submit"
+        >
+          Korban Mahjong
+        </button>
+      </div>
+    </div>
+  )
+}
